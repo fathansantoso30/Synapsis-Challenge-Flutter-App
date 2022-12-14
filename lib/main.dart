bb44d1aaacf8b5e55044836b70f2da024724152a
fathansantoso30 <fathansantoso30@gmail.com>
2022-12-15 03:43:38 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:synapsis_challenge_app/login_screen.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Login Demo', home: LoginScreen());
+  }
+}
